95c50a1e80d4773e5c45dac338b5d0361ac18a01
BrayanEstevezC <brayan_1321124024@uptecamac.edu.mx>
2023-11-13 00:15:13 -0600

    update 0003.4

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,23 @@ document.addEventListener("DOMContentLoaded", function () {
     var Content = document.getElementById("content");
     var Close = document.getElementById("close");
     var hamburger = document.getElementById("hamburger");
+    var title = document.querySelector(".catalogos");
+    var ubicacion = document.querySelector(".ubicacion");
+    var mantenimiento = document.querySelector(".mantenimiento");
+    var motor = document.querySelector(".motor");
 
 
     Menu.onclick = function () {
-        ColMenu.style.width = "10%";
+        ColMenu.style.width = "16%";
         hamburger.style.justifyContent = "end";
-        Content.style.width = "90%";
+        Content.style.width = "84%";
         Close.classList.remove("none");
         Menu.classList.add("none");
+        Menu.classList.add("none");
+        title.classList.remove("none");
+        ubicacion.classList.remove("none");
+        mantenimiento.classList.remove("none");
+        motor.classList.remove("none");
 
     };
     Close.onclick = function () {
@@ -21,5 +30,9 @@ document.addEventListener("DOMContentLoaded", function () {
         Content.style.width = "95%";
         Close.classList.add("none");
         Menu.classList.remove("none");
+        title.classList.add("none");
+        ubicacion.classList.add("none");
+        mantenimiento.classList.add("none");
+        motor.classList.add("none");
     };
 });
